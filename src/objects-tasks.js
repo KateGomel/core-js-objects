eaf776d1da4f0b0af73333c687d63aec3338567c
KateGomel <ykatuay@gmail.com>
2025-03-06 21:04:42 +0300

    feat: add solutions  removeProperties & compareObjects

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -55,8 +55,14 @@ function mergeObjects(objects) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, ['age']) => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const copyObj = { ...obj };
+
+  keys.forEach((key) => {
+    delete copyObj[key];
+  });
+
+  return copyObj;
 }
 
 /**
@@ -71,8 +77,10 @@ function removeProperties(/* obj, keys */) {
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 2}) => true
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 3}) => false
  */
-function compareObjects(/* obj1, obj2 */) {
-  throw new Error('Not implemented');
+function compareObjects(obj1, obj2) {
+  const keys1 = Object.keys(obj1);
+
+  return keys1.every((key) => obj1[key] === obj2[key]);
 }
 
 /**
